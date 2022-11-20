0d4b944da935e09520e3766dccde23f5bbc2d098
Carly Nelson <chermansen723@gmail.com>
2022-11-20 01:35:44 +0000

    Next and previous buttons reliably working

--- front-end_/src/ArtForm.js
+++ front-end_/src/ArtForm.js
@@ -19,37 +19,46 @@ function ArtForm() {
     const [results, setResults] = useState("");
     const [pieceUpdated, setPieceUpdated] = useState(true);
     const [collectionSize, setCollectionSize] = useState("");
-    const [displayPieceID, setDisplayPieceID] = useState(437133);
+    const [displayPieceID, setDisplayPieceID] = useState(2000);
     const [saved, setSaved] = useState([]);
     const [collection, setCollection] = useState([]);
-
-    const currentIndex = useRef(0);
+    
+    const currentIndex = useRef(-1);
     
     //Get next id, indexes through my list from the collection
     
-    const fetchArtCollection = async() => {
+    function fetchArtCollection() {
         try {
             const url = "https://collectionapi.metmuseum.org/public/collection/v1/objects";
-            const response = await axios.get(url);
+            const response = axios.get(url);
             console.log("Fetching Collection");
-            console.log(response);
-            setCollection(response.data.objectIDs);
-            console.log(response.data.objectIDs);
-            setCollectionSize(response.data.total);
-           // console.log("collection");
-            //console.log(collection);
-           // collectionSize = response.data.total;
-            console.log("Collection Size" + collectionSize);
-            
+            return response;
         }
         catch(error) {
             setError("error retrieving art collection" + error);
         }
     };
     
-    function updateIndex() {
-        if (currentIndex < collection.length) {
-            currentIndex = currentIndex + 1;
+    const handleFetchCollection = async() => { 
+                try {
+                    fetchArtCollection().then (
+                        (response) => {
+                            console.log(response);
+                            setCollection(response.data.objectIDs);
+                            return response;
+                        }
+                    );
+                }
+                
+                catch(error) {
+                    setError("error handling art collection" + error);
+                }
+            };
+    
+    function increaseIndex() {
+        console.log("collection length" + collection.length);
+        if (currentIndex.current < collection.length) {
+            currentIndex.current = currentIndex.current + 1;
             return currentIndex;
         }
         else {
@@ -57,27 +66,43 @@ function ArtForm() {
         }
     }
     
-    
+    function decreaseIndex() {
+        console.log("collection length" + collection.length);
+        if (currentIndex.current > 0) {
+            currentIndex.current = currentIndex.current - 1;
+            return currentIndex;
+        }
+        else {
+            return currentIndex;
+        }
+    }
     
     const handleNext = (e) => {
         e.preventDefault();
         console.log("In next print collection");
-        console.log(collection);
-        updateIndex();
+        console.log(collection); 
+        if (currentIndex.current === -1) {
+            const index = collection.findIndex(element => element === 2000);
+            currentIndex.current = index;
+        } 
+      
+        increaseIndex(); 
     
-        setDisplayPieceID(collection[currentIndex]);
-        //updatePiece();
+    
+        setDisplayPieceID(collection[currentIndex.current]);
     };
     
     const handlePrevious = (e) => {
-        e.preventDefault();
-        updatePiece();
-    }
+       e.preventDefault();
+        console.log("In previous");
+        decreaseIndex();
+        setDisplayPieceID(collection[currentIndex.current]);
+    };
     
     
     const updatePiece = () => {
         setPieceUpdated(true);
-        }
+    };
     
     
     /*const handleSubmit = (e) => {
@@ -93,7 +118,6 @@ function ArtForm() {
     
     const addSave = async(e) => { //Need to learn how to connect this to another component
         e.preventDefault();
-        
         try {
             await axios.post("/api/saved", {displayPieceID});
 
@@ -105,38 +129,30 @@ function ArtForm() {
     
     
     useEffect(() => {
-    
-        /*if (pieceUpdated) {
-            console.log("Inside updating piece");
-            fetchDisplayPiece();
-            setPieceUpdated(false);
-        }*/
-    
-        //fetchArtCollection(); put in its own use effect later
-    },[displayPieceID]); //pieceUpdated
+        
+    },[displayPieceID, collection.length]);
     
     useEffect(() => {
-        fetchArtCollection();
+        handleFetchCollection();
     },[]);
     
-            return (
-                <div className = "page">
-                    {error}
-                    {collectionSize !== 0 ? <h1>Enjoy {collectionSize} pieces of art from the Met</h1> : <h1> Enjoy many pieces of art from the Met </h1>} 
-                    <h3>Find your new favorite art </h3>
-                    <form className="art-search">
-                        <legend>Search through the whole museum or a specific department</legend>
-                        
-                    </form>
-                    <div> {results}
-                        <DisplayPiece displayPieceID={displayPieceID}/>
-                        <a href="#" className="previous round" onClick={e => handlePrevious(e)}>&#8249;</a>
-                        <a href="#" className="next round" onClick={e => handleNext(e)}>&#8250;</a>
-                        <button className="save" onClick={e => addSave(e)}>Save</button>
-                    </div>
-                </div> 
-            );
-    }
+    return (
+        <div className = "page">
+            {error}
+            {collectionSize !== 0 ? <h1>Enjoy {collectionSize} pieces of art from the Met</h1> : <h1> Enjoy many pieces of art from the Met </h1>} 
+            <h3>Find your new favorite art </h3>
+            <form className="art-search">
+                <legend>Search through the whole museum or a specific department</legend>
+            </form>
+            <div> {results}
+                <DisplayPiece displayPieceID={displayPieceID}/>
+               {collection.length != 0 ? <a href="#" className="previous round" onClick={e => handlePrevious(e)}>&#8249;</a> : <p> Loading data </p>}
+                {collection.length != 0 ? <a href="#" className="next round" onClick={e => handleNext(e)}>&#8250;</a> : <p> Loading data </p>}
+                <button className="save" onClick={e => addSave(e)}>Save</button>
+            </div> 
+        </div> 
+    );
+}
 export default ArtForm; 
 
 //                        {displayPiece.data ? <DisplayPiece artpiece={displayPiece} displayPieceID={displayPieceID}/> : <div></div>}
--- front-end_/src/DisplayPiece.js
+++ front-end_/src/DisplayPiece.js
@@ -8,10 +8,9 @@ function DisplayPiece(props){
         const [displayPiece, setDisplayPiece] = useState({});
         const [error, setError] = useState("");
     
-        function fetchAndSetDisplayPiece() {
+        function fetchDisplayPiece() {
             const url = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + props.displayPieceID;//props.displayPieceID.current
             const response =  axios.get(url);
-            //const str = JSON.stringify(response);
             return response;
         }
         
@@ -19,7 +18,7 @@ function DisplayPiece(props){
          const handleDisplayPiece = async() => { 
                 try {
                     
-                    fetchAndSetDisplayPiece().then (
+                    fetchDisplayPiece().then (
                         (response) => {
                             console.log(response);
                             
@@ -27,8 +26,28 @@ function DisplayPiece(props){
                                 response.data.artistDisplayName = "Artist unknown";
                             }
                 
-                            if (response.data.primaryImage === "") {
-                                response.data.primaryImage = "Image unavailable";
+                            if (response.data.primaryImageSmall === "") {
+                                response.data.primaryImageSmall = "Image unavailable";
+                            }
+                            
+                            if (response.data.title === "") {
+                                response.data.title = "No title";
+                            }
+                            
+                            if (response.data.culture === "") {
+                                response.data.culture = "culture unknown";
+                            }
+                            
+                            if (response.data.city === "") {
+                                response.data.city = "city unknown";
+                            }
+                            
+                            if (response.data.country === "") {
+                                response.data.country = "country unknown";
+                            }
+                            
+                            if (response.data.objectDate === "") {
+                                response.data.objectDate = "date unknown";
                             }
                             
                             setDisplayPiece(response);
@@ -52,9 +71,9 @@ function DisplayPiece(props){
            <div>
            {error}
                 {displayPiece.data  ? <p className="artist">{displayPiece.data.artistDisplayName}</p> : <p> Artist Name: loading </p>}
-                {displayPiece.data ? <img className="artImg"src={displayPiece.data.primaryImage}/> : <p> Art Image: loading </p>}
-               
-
+                {displayPiece.data ? <img className="artImg"src={displayPiece.data.primaryImageSmall}/> : <p> Art Image: loading </p>}
+                {displayPiece.data  ? <p className="description">{displayPiece.data.title}</p> : <p> Artwork Name: loading </p>}
+                {displayPiece.data  ? <p className="culture">{displayPiece.data.culture}, created in {displayPiece.data.city}, {displayPiece.data.country} in the years {displayPiece.data.objectDate}</p> : <p> Artwork Details: loading </p>}
             </div> 
         ); // {(displayPiece.data.primaryImage === "Image unavailable")? <p>Image unavailable</p> : <p></p>}
         
