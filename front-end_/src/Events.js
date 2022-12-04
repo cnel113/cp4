dd78b9bf71552e02b2542355f8630edb5018fdb7
Carly Nelson <chermansen723@gmail.com>
2022-12-04 03:22:29 +0000

    finished Events page

--- front-end_/src/Events.js
+++ front-end_/src/Events.js
@@ -2,51 +2,51 @@ import React from 'react';
 import './Calendar.css';
 import Calendar from 'react-calendar'; 
 import { useState, useEffect, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import classImg from "./EventsImgs/classes.jpg" 
+import exhibitionImg from "./EventsImgs/exhibition.jpg" 
+import kidImg from "./EventsImgs/kids.jpg" 
+
 
 const Events = () => {
     const [date, setDate] = useState(new Date())
   return (
     <div className="events-page">
     
-      
       <h1>Attend Local Events in Your Community</h1>
       <p className="subtitle">The best way to experience art is to make it yourself.</p>
       <p>Check out our calendar of large events</p>
+      
       <div className="calendar-container">
         <Calendar onChange={setDate} value={date}/>
-      </div>
-      <p>Selected date: {date.toDateString()}</p>
-      
-      <div className="bio">
-        <img src="#"/>
+        <p>Selected date: {date.toDateString()}</p>
       </div>
       
-      <div className="horizonal-container">
-        <div className="feature tile">
+        <div className="feature">
           <h2>Free or Reduced Cost Classes</h2>
-          <img src="#"/>
-          <p className="subtitle">Everyone should have access to fine art. No matter what in-person opportunities are available, 
-          everyone can experience the fine art of the Met on our explore page </p>
-          <NavLink to="/explore" className="redirect-button">Explore</NavLink>
+          <img src={classImg}/>
+          <p className="subtitle">Qualifying individuals get professional art training for free</p>
+          <p>If you want to learn how to create art, you should be able to regardless of your circumstance. 
+          In select cities we offer free or subsidized classes for people who are low-income, disabled, or have other needs.</p>
+          <button className="inline-link">See Availability</button>
         </div>
         
-        <div className="feature tile">
-          <h2>Share Your Creations</h2>
-          <img src="#"/>
-          <p className="subtitle">Many future masterpieces come from small-time artists, many of whom are in remote locations. 
-          Get inspired or share your work on our community blog.</p>
-          <NavLink to="/about" className="redirect-button">Share</NavLink>
+        <div className="feature">
+          <h2>Community Exhibitions</h2>
+          <img src={exhibitionImg}/>
+          <p className="subtitle">Artists have the opportunity to share art in their local areas.</p>
+          <p>Small-town artists and those in less developed areas often lack opportunties for exposure. 
+          We solve this by hosting exhibitions across the world where artists can display their art and compete for prizes.</p>
+          <button className="inline-link">Dates & Times</button>
         </div>
   
-        <div className="feature tile">
-          <h2>Community Events</h2>
-          <img src="#"/>
-          <p className="subtitle">We partner with local organizations in the Unites States to offer free and reduced cost art classes 
-          to under priveldged individuals. Check your eligibility and sign up for events here.</p>
-          <NavLink to="/about" className="redirect-button">Events</NavLink>
+        <div className="feature">
+          <h2>Children's Events</h2>
+          <img src={kidImg}/>
+          <p className="subtitle">Kids get the opportunity to develop their love for art with hands on experience.</p>
+          <p>When it comes to art, you can't leave out the kids. We support budding artists with 
+          our low-priced curriculums. With supplies and engaging video lessons your kids can explore the world of art.</p>
+          <button className="inline-link">Products & Pricing</button>
         </div>
-      </div>
     </div>
   );
 }
